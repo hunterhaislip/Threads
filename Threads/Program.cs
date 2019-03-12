448737ce13eaff69e81bc9d367fec6470251ae16
Hunter Haislip <hunlhais@ut.utm.edu>
2019-03-12 13:54:44 -0500

    Push

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Threads
 {
+    class FindPiThread
+    {
+        public int darts;
+        public int count;
+        Random Throws = new Random();
+
+        FindPiThread(int n)
+        {
+
+            darts = 0;
+            count = 0;
+        }
+    }
+        
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
