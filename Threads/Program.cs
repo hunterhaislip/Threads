abd14485642b64a83f847caf0ecb2db9d25974fd
Hunter Haislip <hunterhaislip@gmail.com>
2019-03-14 12:03:05 -0500

    Final Push

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Author: Hunter Haislip
+ * Date: 3/14/19
+ * File: Program.cs
+ * Description: Simple PI project
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,20 +14,21 @@ using System.Threading.Tasks;
 
 namespace Threads
 {
-    class FindPiThread
+    class FindPiThread // First FindPiThread Class
     {
         public int darts;
         public int count;
         public Random Throws;
+        //Public so they can all be accessed
 
-        public FindPiThread(int t)
+        public FindPiThread(int t) // Constructor that initializes the constructor
         {
             Throws = new Random();
             darts = t;
             count = 0;
         }
 
-        public void throwDarts()
+        public void throwDarts() // Function that throws the darts
         {
             double x, y;
             for (int i = 0; i < darts; i++)
@@ -28,14 +36,13 @@ namespace Threads
                 x = Throws.NextDouble();
                 y = Throws.NextDouble();
 
-                if(x*x + y*y <= 1)
+                if((x*x + y*y) <= 1) // Math to know when to run if statement
                 {
                     count++;
                 }
             }
         }
     }
-        
 
 
     class Program
@@ -44,6 +51,7 @@ namespace Threads
         {
             int Ask1;
             int Ask2;
+            // Asks the user how many darts and threads
             Console.WriteLine("How many darts would you like a thread to throw?");
             Ask1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many threads would you like to have?");
@@ -58,10 +66,27 @@ namespace Threads
                 List2.Add(Add1);
                 Thread Thread1 = new Thread(new ThreadStart(Add1.throwDarts));
                 List1.Add(Thread1);
-                List1.Start(); ///////GUYHEFDHYGVHHJFDEBGVHJDEFBGJHBHJFDHJGHJDSFHJBGJHBFDHJB
-                Thread.Sleep(16);
+                List1[i].Start(); 
+                Thread.Sleep(16); // Makes sure all threads get a value
             }
 
+            foreach(Thread item in List1)
+            {
+                item.Join();
+            }
+
+            double total = 0;
+            foreach(FindPiThread item in List2)
+            {
+                double Try1;
+                double Try2;
+                Try1 = item.count;
+                Try2 = (Try1 / Ask1) * 4; // Math to find PI
+                total = Try2;
+            }
+
+            Console.WriteLine("Approx Pi " + total); // Output for the approx of PI
+            Console.ReadKey();
         }
     }
 }
